a137ce21c2d6b95ba3cf59765083f5c1751a50e6
SHUMAILOVALEXANDER <alexander.SHUMAILOVF@gmail.com>
2025-01-28 20:04:53 +0300

    Задание_2.py

--- Лабораторная 2/Задание_2.py
+++ Лабораторная 2/Задание_2.py
@@ -1,4 +1,4 @@
-BOOKS_DATABASE = [
+books_database = [
     {
         "id": 1,
         "name": "test_name_1",
@@ -11,20 +11,16 @@ BOOKS_DATABASE = [
     }
 ]
 
-
 class Book:
     def __init__(self, id_, name, pages):
         self.id = id_
         self.name = name
         self.pages = pages
-
     def __str__(self):
         return f'Книга "{self.name}"'
 
     def __repr__(self):
         return f"Book(id_={self.id}, name='{self.name}', pages={self.pages})"
-
-
 class Library:
     def __init__(self, books=None):
         if books is None:
@@ -47,20 +43,16 @@ class Library:
 
     def __repr__(self):
         return f"Library(books={self.books})"
-
-
 if __name__ == 'Класс Library':
     empty_library = Library()
     print(empty_library.get_next_book_id())
 
     list_books = [
         Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"])
-        for book_dict in BOOKS_DATABASE
+        for book_dict in books_database
     ]
-
     library_with_books = Library(books=list_books)
     print(library_with_books.get_next_book_id())
-
     try:
         index = library_with_books.get_index_by_book_id(1)
         print(f"{index}")
